c4d192b46a2f848ffc38ec882b7d5c42d8887f78
Lukas Dirda <lukas_dirda@epam.com>
2021-08-04 10:00:18 +0300

    added homework task for scope-hoisting-closures

---
+++ Modules/JavaScript/Scope-Hoisting-Closures/homework.js
@@ -0,0 +1,57 @@
+console.log(`
+  Task 1.
+    Create a function runningAverage() that returns a callable function object.
+    Update the series with each given value and calculate the current average.
+
+    rAvg = runningAverage();
+    rAvg(10) = 10.0;
+    rAvg(11) = 10.5;
+    rAvg(12) = 11;
+`);
+
+const runningAverage = () => {
+  let list = [];
+
+  return (newVal) => {
+    if (newVal) {
+      list.push(newVal);
+  
+      return list.reduce((a, b) => a + b, 0) / list.length;
+    }
+  };
+};
+
+let rAvg = runningAverage();
+console.log(rAvg(10));
+console.log(rAvg(11));
+console.log(rAvg(12));
+
+
+console.log('----------');
+console.log(`
+  Task 2.
+    Write a sum function which will work properly when invoked using syntax below.
+
+    sum(2, 3); // Outputs	5
+    sum(2)(3); // Outputs	5
+    sum(1)(2)(3)(4); // Outputs	10
+
+    *** Locally it seems that there is an issue with JS. Works fine with \`jsfiddle\`[https://jsfiddle.net/47aubz9j/]
+`);
+
+const sum = (...arg1) => {
+  let currentSum = arg1.reduce((a, b) => a + b, 0);
+
+  const f = (...arg2) => {
+    currentSum += arg2.reduce((a, b) => a + b, 0);
+    return f;
+  };
+
+  f.toString = () => currentSum.toString();
+
+  return f;
+}
+
+console.log(sum(2, 3));
+console.log(sum(2)(3));
+console.log(sum(1)(2)(3)(4));
--- README.md
+++ README.md
@@ -4,9 +4,14 @@
 
 ## Installation
 
-  1. Install NodeJS[https://nodejs.org/en/download/]
+  1. Install [NodeJS](https://nodejs.org/en/download/)
 
   2. Install npm `npm i -g npm`
 
   3. Install repo dependencies `npm i` 
 
+## Running Javascript Tasks
+
+  `npx babel-node [file]`
+
+  example: `npx babel-node Modules/JavaScript/Scope-Hoisting-Closures/homework.js`
